9b7139a9021072ef37617da4d2c67492af3c8b7e
pbrats <p.bratsioti@gmail.com>
2024-04-12 19:58:32 +0300

    step1 first layout created step2 initialization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { StartComponent } from './component/start/start.component';
 import { Step1Component } from './component/step1/step1.component';
+import { Step2Component } from './component/step2/step2.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "step0", pathMatch: "full" },
     { path: "step0", component: StartComponent},
-    { path: "step1", component: Step1Component}
+    { path: "step1", component: Step1Component},
+    { path: "step2", component: Step2Component}
 ];
---
+++ src/app/component/step2/step2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step2',
+  standalone: true,
+  imports: [],
+  templateUrl: './step2.component.html',
+  styleUrl: './step2.component.css'
+})
+export class Step2Component {
+
+}
