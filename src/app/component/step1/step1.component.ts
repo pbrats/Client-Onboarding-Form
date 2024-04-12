80c56dafd8dd1449ff58d76b0ba21786f5a1f404
pbrats <p.bratsioti@gmail.com>
2024-04-13 01:07:27 +0300

    class.active added to selected buttons active button changing fixed dropdowns added on case step2 first layout created saved images added to elements svgs added

--- src/app/component/step1/step1.component.ts
+++ src/app/component/step1/step1.component.ts
@@ -9,5 +9,154 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './step1.component.css'
 })
 export class Step1Component {
+  buttonDontKnow: boolean = false;
+  buttonNo: boolean = false;
+  buttonYes: boolean = false;
+  buttonSame: boolean = false;
+  buttonDifferent: boolean = false;
+  buttonOut: boolean = false;
+  buttonWall: boolean = false;
+  buttonStand : boolean = false;
+  buttonDontKnowMeters: boolean = false;
+  button0Meters: boolean = false;
+  button5Meters: boolean = false;
+  button10Meters: boolean = false;
+  button15Meters : boolean = false;
 
+  clickedDontKnow(): void {
+    if (this.buttonDontKnow) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonDontKnow = true;
+      this.buttonYes = false;
+      this.buttonNo = false;
+
+    }
+  }
+  clickedYes(): void {
+    if (this.buttonYes) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonYes = true;
+      this.buttonDontKnow = false;
+      this.buttonNo = false;
+
+    }
+  }
+  clickedNo(): void {
+    if (this.buttonNo) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonNo = true;
+      this.buttonYes = false;
+      this.buttonDontKnow = false;
+
+    }
+  }
+  clickedSame(): void {
+    if (this.buttonSame) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonSame = true;
+      this.buttonDifferent = false;
+      this.buttonOut = false;
+
+    }
+  }
+  clickedDifferent(): void { 
+    if (this.buttonDifferent) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonDifferent = true;
+      this.buttonSame = false;
+      this.buttonOut = false;
+
+    }
+  }
+  clickedOut(): void {
+    if (this.buttonOut) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonOut = true;
+      this.buttonDifferent = false;
+      this.buttonSame = false;
+
+    }
+  }
+  clickedWall(): void {
+    if (this.buttonWall) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonWall = true;
+      this.buttonStand = false;
+      }
+  }
+  clickedStand(): void {
+    if (this.buttonStand) {
+     
+    } else { // if it is inactive it activates 
+      this.buttonStand = true;
+      this.buttonWall = false;
+      }
+  }
+  clickedDontKnowMeters(): void {
+    if (this.buttonDontKnowMeters) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonDontKnowMeters = true;
+      this.button0Meters = false;
+      this.button5Meters = false;
+      this.button10Meters = false;
+      this.button15Meters = false;
+
+    }
+  }
+  clicked0Meters(): void {
+    if (this.button0Meters) {
+      
+    } else {// if it is inactive it activates 
+      this.button0Meters = true;
+      this.button5Meters = false;
+      this.button10Meters = false;
+      this.button15Meters = false;
+      this.buttonDontKnowMeters = false;
+
+    }
+  }
+  clicked5Meters(): void {
+    if (this.button5Meters) {
+      
+    } else {// if it is inactive it activates 
+      this.button5Meters = true;
+      this.button0Meters = false;
+      this.buttonDontKnowMeters = false;
+      this.button10Meters = false;
+      this.button15Meters = false;
+
+    }
+  }
+  clicked10Meters(): void {
+    if (this.button10Meters) {
+      
+    } else {// if it is inactive it activates 
+      this.button10Meters = true;
+      this.button0Meters = false;
+      this.button5Meters = false;
+      this.buttonDontKnowMeters = false;
+      this.button15Meters = false;
+
+    }
+  }
+  clicked15Meters(): void {
+    if (this.button15Meters) {
+      
+    } else {// if it is inactive it activates 
+      this.button15Meters = true;
+      this.button0Meters = false;
+      this.button5Meters = false;
+      this.button10Meters = false;
+      this.buttonDontKnowMeters = false;
+
+    }
+  }
 }
--- src/app/component/step2/step2.component.ts
+++ src/app/component/step2/step2.component.ts
@@ -8,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrl: './step2.component.css'
 })
 export class Step2Component {
+  buttonDontKnow: boolean = false;
+  buttonOption1: boolean = false;
+  buttonOption2: boolean = false;
+  clickedDontKnow(): void {
+    if (this.buttonDontKnow) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonDontKnow = true;
+      this.buttonOption1 = false;
+      this.buttonOption2 = false;
 
+    }
+  }
+  clickedOption1(): void {
+    if (this.buttonOption1) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonOption1 = true;
+      this.buttonDontKnow = false;
+      this.buttonOption2 = false;
+
+    }
+  }
+  clickedOption2(): void {
+    if (this.buttonOption2) {
+      
+    } else {// if it is inactive it activates 
+      this.buttonOption2 = true;
+      this.buttonOption1 = false;
+      this.buttonDontKnow = false;
+
+    }
+  }
 }
