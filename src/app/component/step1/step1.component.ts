f8e42b6e50e2f5bec58b16141f302044d8b1c420
pbrats <p.bratsioti@gmail.com>
2024-05-11 20:03:10 +0300

    step1 store all selections in an object step1 instead of many variables

--- src/app/component/step1/step1.component.ts
+++ src/app/component/step1/step1.component.ts
@@ -52,14 +52,27 @@ export class Step1Component {
     this.languageService.language$.subscribe(language => {
       this.language = language;
     });
-    this.selectedButton1Value = sessionStorage.getItem("selectedButton1");
-    this.selectYesValue = sessionStorage.getItem("selectYes");
-    this.selectNoValue = sessionStorage.getItem("selectNo");
-    this.selectedButton2Value = sessionStorage.getItem("selectedButton2");
-    this.selectedButton3Value = sessionStorage.getItem("selectedButton3");
-    this.selectedButton4Value = sessionStorage.getItem("selectedButton4");
-    if (this.selectedButton1Value !== null && this.selectedButton2Value !== null && this.selectedButton3Value !== null && this.selectedButton4Value !== null) {
-      if (this.selectYesValue !== null) {
+    const step1Stored = sessionStorage.getItem("step1");
+    if (step1Stored !== null) {
+      const step1 = JSON.parse(step1Stored);
+      console.log("step1 onInit: ", step1);
+
+      this.selectedButton1Value = step1["selectedButton1"];
+      this.selectYesValue = step1["selectYes"];
+      this.selectNoValue = step1["selectNo"];
+      this.selectedButton2Value = step1["selectedButton2"];
+      this.selectedButton3Value = step1["selectedButton3"];
+      this.selectedButton4Value = step1["selectedButton4"];
+    }
+    console.log("this.selectedButton1Value", this.selectedButton1Value);
+    // this.selectedButton1Value = sessionStorage.getItem("selectedButton1");
+    // this.selectYesValue = sessionStorage.getItem("selectYes");
+    // this.selectNoValue = sessionStorage.getItem("selectNo");
+    // this.selectedButton2Value = sessionStorage.getItem("selectedButton2");
+    // this.selectedButton3Value = sessionStorage.getItem("selectedButton3");
+    // this.selectedButton4Value = sessionStorage.getItem("selectedButton4");
+    if (this.selectedButton1Value !== undefined && this.selectedButton2Value !== undefined && this.selectedButton3Value !== undefined && this.selectedButton4Value !== undefined) {
+      if (this.selectYesValue !== undefined) {
         this.step1Form = this.formBuilder.group({
           selectedButton1: [this.selectedButton1Value, Validators.required],
           selectYes: [this.selectYesValue, Validators.required],
@@ -68,7 +81,7 @@ export class Step1Component {
           selectedButton3: [this.selectedButton3Value, Validators.required],
           selectedButton4: [this.selectedButton4Value, Validators.required]
         });
-      } else if (this.selectNoValue !== null) {
+      } else if (this.selectNoValue !== undefined) {
         this.step1Form = this.formBuilder.group({
           selectedButton1: [this.selectedButton1Value, Validators.required],
           selectYes: [''],
@@ -77,7 +90,7 @@ export class Step1Component {
           selectedButton3: [this.selectedButton3Value, Validators.required],
           selectedButton4: [this.selectedButton4Value, Validators.required]
         });
-      }else{
+      } else {
         this.step1Form = this.formBuilder.group({
           selectedButton1: [this.selectedButton1Value, Validators.required],
           selectYes: [''],
@@ -107,16 +120,16 @@ export class Step1Component {
         this.buttonWall = true;
       } else if (this.selectedButton3Value === 'buttonStand') {
         this.buttonStand = true;
-      } 
+      }
       if (this.selectedButton4Value === 'buttonDontKnowMeters') {
         this.buttonDontKnowMeters = true;
       } else if (this.selectedButton4Value === 'button0Meters') {
         this.button0Meters = true;
-      }else if (this.selectedButton4Value === 'button5Meters') {
+      } else if (this.selectedButton4Value === 'button5Meters') {
         this.button5Meters = true;
       } else if (this.selectedButton4Value === 'button10Meters') {
         this.button10Meters = true;
-      }else {
+      } else {
         this.button15Meters = true;
       }
     } else {
@@ -138,34 +151,112 @@ export class Step1Component {
     this.selectedButton1Value = this.step1Form.get("selectedButton1")?.value;
     if (this.selectedButton1Value !== '') {
       console.log("selected button1 value on submit : ", this.selectedButton1Value);
-      sessionStorage.setItem("selectedButton1", this.selectedButton1Value);
+      const step1Stored = sessionStorage.getItem("step1");
+      if (step1Stored !== null) {
+        const step1 = JSON.parse(step1Stored);
+        console.log(step1);
+        console.log(typeof (step1));
+        step1['selectedButton1'] = this.selectedButton1Value;
+        sessionStorage.setItem("step1", JSON.stringify(step1));
+      } else {
+        const newItem = { 'selectedButton1': this.selectedButton1Value };
+        console.log("new item button1 :", newItem);
+        console.log(typeof (newItem));
+        sessionStorage.setItem("step1", JSON.stringify(newItem));
+      }
+      // sessionStorage.setItem("selectedButton1", this.selectedButton1Value);
     }
     if (this.selectedYes) {
       this.selectYesValue = this.step1Form.get("selectYes")?.value;
       console.log("selectYes on submit : ", this.selectYesValue);
-      sessionStorage.setItem("selectYes", this.selectYesValue);
-      sessionStorage.removeItem("selectNo");
+      const step1Stored = sessionStorage.getItem("step1");
+      if (step1Stored !== null) {
+        const step1 = JSON.parse(step1Stored);
+        console.log(step1);
+        console.log(typeof (step1));
+        step1['selectYes'] = this.selectYesValue;
+        if (step1['selectNo'] !== null) {
+          delete step1['selectNo'];
+          console.log('selectNo deleted');
+          console.log("step1 new:", step1);
+        }
+        sessionStorage.setItem("step1", JSON.stringify(step1));
+      }
+      // sessionStorage.setItem("selectYes", this.selectYesValue);
+      // sessionStorage.removeItem("selectNo");
     }
     if (this.selectedNo) {
       this.selectNoValue = this.step1Form.get("selectNo")?.value;
       console.log("selectNo on submit : ", this.selectNoValue);
-      sessionStorage.setItem("selectNo", this.selectNoValue);
-      sessionStorage.removeItem("selectYes");
+      const step1Stored = sessionStorage.getItem("step1");
+      if (step1Stored !== null) {
+        const step1 = JSON.parse(step1Stored);
+        console.log(step1);
+        console.log(typeof (step1));
+        step1['selectNo'] = this.selectNoValue;
+        if (step1['selectYes'] !== null) {
+          delete step1['selectYes'];
+          console.log('selectYes deleted');
+          console.log("step1 new:", step1);
+        }
+        sessionStorage.setItem("step1", JSON.stringify(step1));
+      }
+      // sessionStorage.setItem("selectNo", this.selectNoValue);
+      // sessionStorage.removeItem("selectYes");
     }
     this.selectedButton2Value = this.step1Form.get("selectedButton2")?.value;
     if (this.selectedButton2Value !== '') {
       console.log("selected button2 value on submit : ", this.selectedButton2Value);
-      sessionStorage.setItem("selectedButton2", this.selectedButton2Value);
+      const step1Stored = sessionStorage.getItem("step1");
+      if (step1Stored !== null) {
+        const step1 = JSON.parse(step1Stored);
+        console.log(step1);
+        console.log(typeof (step1));
+        step1['selectedButton2'] = this.selectedButton2Value;
+        sessionStorage.setItem("step1", JSON.stringify(step1));
+      } else {
+        const newItem = { 'selectedButton2': this.selectedButton2Value };
+        console.log("new item button2 :", newItem);
+        console.log(typeof (newItem));
+        sessionStorage.setItem("step1", JSON.stringify(newItem));
+      }
+      // sessionStorage.setItem("selectedButton2", this.selectedButton2Value);
     }
     this.selectedButton3Value = this.step1Form.get("selectedButton3")?.value;
     if (this.selectedButton3Value !== '') {
       console.log("selected button3 value on submit : ", this.selectedButton3Value);
-      sessionStorage.setItem("selectedButton3", this.selectedButton3Value);
+      const step1Stored = sessionStorage.getItem("step1");
+      if (step1Stored !== null) {
+        const step1 = JSON.parse(step1Stored);
+        console.log(step1);
+        console.log(typeof (step1));
+        step1['selectedButton3'] = this.selectedButton3Value;
+        sessionStorage.setItem("step1", JSON.stringify(step1));
+      } else {
+        const newItem = { 'selectedButton3': this.selectedButton3Value };
+        console.log("new item button3 :", newItem);
+        console.log(typeof (newItem));
+        sessionStorage.setItem("step1", JSON.stringify(newItem));
+      }
+      // sessionStorage.setItem("selectedButton3", this.selectedButton3Value);
     }
     this.selectedButton4Value = this.step1Form.get("selectedButton4")?.value;
     if (this.selectedButton4Value !== '') {
       console.log("selected button4 value on submit : ", this.selectedButton4Value);
-      sessionStorage.setItem("selectedButton4", this.selectedButton4Value);
+      const step1Stored = sessionStorage.getItem("step1");
+      if (step1Stored !== null) {
+        const step1 = JSON.parse(step1Stored);
+        console.log(step1);
+        console.log(typeof (step1));
+        step1['selectedButton4'] = this.selectedButton4Value;
+        sessionStorage.setItem("step1", JSON.stringify(step1));
+      } else {
+        const newItem = { 'selectedButton4': this.selectedButton4Value };
+        console.log("new item button4 :", newItem);
+        console.log(typeof (newItem));
+        sessionStorage.setItem("step1", JSON.stringify(newItem));
+      }
+      // sessionStorage.setItem("selectedButton4", this.selectedButton4Value);
     }
     if (this.step1Form.valid) {
       this.router.navigate(['step2']);
@@ -241,7 +332,6 @@ export class Step1Component {
       this.buttonSame = true;
       this.buttonDifferent = false;
       this.buttonOut = false;
-     
     }
     if (buttonName == "buttonDifferent") {
       console.log("button Different");
