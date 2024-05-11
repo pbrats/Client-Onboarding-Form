e98aa15d34b53df8386cc4fa6123519957ef3b62
pbrats <p.bratsioti@gmail.com>
2024-05-11 23:26:53 +0300

    step3 form fixed with validators and messages appearing on error on submit users infos are saved in sessionstorage

--- src/app/component/step3/step3.component.ts
+++ src/app/component/step3/step3.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { LanguageService } from '../../service/language.service';
 import { ThemeService } from '../../service/theme.service';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-step3',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule, ReactiveFormsModule],
   templateUrl: './step3.component.html',
   styleUrl: './step3.component.css'
 })
@@ -14,20 +15,45 @@ export class Step3Component {
   submitted: boolean = false;
   language!: string;
   darkTheme!: boolean;
+  step3Form!: FormGroup;
+  step3values: any;
   constructor(private router: Router, private languageService: LanguageService, private themeService: ThemeService) { }
-  ngOnInit(){
+  ngOnInit() {
     this.themeService.darkTheme$.subscribe(darkTheme => {
       this.darkTheme = darkTheme;
     });
     this.languageService.language$.subscribe(language => {
       this.language = language;
     });
+    this.setFormValues();
+  }
+  setFormValues() {
+    this.step3Form = new FormGroup({
+      name: new FormControl("", [Validators.required]),
+      lastname: new FormControl("", [Validators.required]),
+      telephone: new FormControl("", [Validators.required, Validators.pattern("[0-9+()-.x]*"), Validators.minLength(7), Validators.maxLength(20)]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      address: new FormControl("", [Validators.required]),
+      postalCode: new FormControl("", [Validators.required]),
+      city: new FormControl("", [Validators.required]),
+      referralCode: new FormControl(""),
+      newsletter: new FormControl(""),
+      invalidCheck: new FormControl("", [Validators.required])
+    });
   }
   onSubmit() {
-    this.submitted = true;
-    setTimeout(() => {
-      this.router.navigate(['end']);
-    }, 2000);
+    this.step3Form.markAllAsTouched();
+    if (this.step3Form.valid) {
+      console.log("form valid");
+      this.step3values = this.step3Form.value;
+      console.log("step3values :", this.step3values);
+      sessionStorage.setItem('user', JSON.stringify(this.step3Form.value));
+      this.submitted = true;
+      setTimeout(() => {
+        this.router.navigate(['end']);
+      }, 2000);
+    }else{
+      console.log("Form invalid!");
+    }
   }
-
 }
