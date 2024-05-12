a92ae537b50a644e99336ed476e1c3857c7a180c
pbrats <p.bratsioti@gmail.com>
2024-05-12 16:14:04 +0300

    email suggestion appears when mail ending is incorrect and on click changes the input value telephone input erases spaces on type

--- src/app/component/step3/step3.component.ts
+++ src/app/component/step3/step3.component.ts
@@ -17,6 +17,9 @@ export class Step3Component {
   darkTheme!: boolean;
   step3Form!: FormGroup;
   step3values: any;
+  emailSuggestionFlag: boolean = false;
+  suggestion: any;
+  suggestionClicked: boolean = false;
   constructor(private router: Router, private languageService: LanguageService, private themeService: ThemeService) { }
   ngOnInit() {
     this.themeService.darkTheme$.subscribe(darkTheme => {
@@ -31,7 +34,7 @@ export class Step3Component {
     this.step3Form = new FormGroup({
       name: new FormControl("", [Validators.required]),
       lastname: new FormControl("", [Validators.required]),
-      telephone: new FormControl("", [Validators.required, Validators.pattern("[0-9+()-.x]*"), Validators.minLength(7), Validators.maxLength(20)]),
+      telephone: new FormControl("", [Validators.required, Validators.pattern('[0-9+()-.x]*'), Validators.minLength(7), Validators.maxLength(20)]),
       email: new FormControl("", [Validators.required, Validators.email]),
       address: new FormControl("", [Validators.required]),
       postalCode: new FormControl("", [Validators.required]),
@@ -39,6 +42,28 @@ export class Step3Component {
       referralCode: new FormControl(""),
       newsletter: new FormControl(""),
       invalidCheck: new FormControl("", [Validators.requiredTrue])
+    });
+    this.step3Form.get('telephone')?.valueChanges.subscribe(value => {
+      if (value) {
+        // Remove any existing spaces
+        let formattedValue = value.replace(/\s/g, '');
+        // Update the value in the form control
+        this.step3Form.get('telephone')?.setValue(formattedValue, { emitEvent: false });
+      }
+    });
+    this.step3Form.get('email')?.valueChanges.subscribe(value => {
+      if (value) {
+        if ((value.split('.')[1] && value.split('.').pop().length >= 3) && !value.endsWith('.com')) {
+          this.emailSuggestionFlag = true;
+          console.log("typed value", value);
+          let part2 = value.split('.')[1].slice(0, 2);
+          let part1 = value.split('.')[0];
+          this.suggestion = part1 + '.' + part2;
+          console.log("suggested value", this.suggestion);
+        } else {
+          this.emailSuggestionFlag = false;
+        }
+      }
     });
   }
   onSubmit() {
@@ -52,8 +77,15 @@ export class Step3Component {
       setTimeout(() => {
         this.router.navigate(['end']);
       }, 2000);
-    }else{
+    } else {
       console.log("Form invalid!");
     }
+  }
+  suggestioncClick() { 
+    this.suggestionClicked = true;
+    console.log("link clicked!");
+    console.log("suggestion clicked :",this.suggestionClicked);
+    this.step3Form.get('email')?.setValue(this.suggestion, { emitEvent: false });
+    this.emailSuggestionFlag = false;
   }
 }
